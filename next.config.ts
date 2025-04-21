b6474c5499900fcdaa81ebb7f7f0926e9a4b652b
Americo Julio Álvarez Téllez <americooficial23@gmail.com>
2025-04-21 17:30:16 -0400

    Construcción de la aplicación para despliegue 3

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   trailingSlash: true,  // Asegura que las URLs terminen en "/"
-  basePath: '/nombre-de-tu-repositorio',  // Reemplaza con el nombre de tu repositorio
-  assetPrefix: '/nombre-de-tu-repositorio/',  // Reemplaza con el nombre de tu repositorio
-  images: {
+ images: {
     domains: ['cataas.com', 'catfact.ninja'], // Agrega los dominios desde los cuales se cargarán imágenes
   },
 };
